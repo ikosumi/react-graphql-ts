5a9c9edc92cd19b5b8925feb55f84d94470d77ed
Ilir Kosumi <i.kosumi@celonis.com>
2020-11-13 19:58:00 +0100

    Implemented graphql crud operations on posts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import {MikroORM} from '@mikro-orm/core';
 import mikroConfig from "./mikro-orm.config";
 import express from "express"
+import {ApolloServer} from "apollo-server-express";
+import {buildSchema} from "type-graphql";
+import {HelloResolver} from "./resolvers/resolver";
+import {PostResolver} from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
     await orm.getMigrator().up();
 
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em })
+    });
+
     const app = express();
+    apolloServer.applyMiddleware({ app });
     app.listen(4000, () => {
         console.log('server started on port 4000');
     });
 
     app.get('/', (_, resp) => {
         resp.send('hello, world');
-    })
+    });
 }
 
 main().catch(err => {
